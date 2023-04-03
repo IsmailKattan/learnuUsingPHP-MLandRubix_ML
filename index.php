bd0b693e667d5fee2a134bb17f47c4b88af734d4
İsmail Kattan <ismail.kattan@ogr.sakarya.edu.tr>
2023-04-03 07:40:21 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="tr-TR">
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <title>Esmail Kattan</title>
+    </head>
+    <body>
+        <div class="container">
+            <h3>Machine Learning nedir</h3>
+            <p>bilgisayarlara açıkça programlanmadan öğrenme yeteneği veren çalışma alanı.</p>
+            <h3>Machine Learning tipleri :</h3>
+            <ul>
+                <li>
+                    <h4>Supervise Learning(Gözetimli öğrenme)</h4>
+                    <p>makine öğrenimi alanında kullanılan bir yöntemdir. Bu yöntemde, makineye verilen veri setindeki her bir örnek için doğru çıktılarla birlikte eğitim verilir</p><p>bunun altında 2 tip vardır:</p>
+                    <ul>
+                        <li>
+                            <h5>Regression(gerilme)</h5>
+                            <ul><li>bu tipte makina sayı tahmin eder </li><li> çok sayıda output mümkündür</li><li>örnek:ev fiyatı tahmini</li></ul>
+                        </li>
+                        <li>
+                            <h5>classification(sınıflandırma)</h5>
+                            <ul><li>bu tipte makine kategori tahmin eder</li><li>az sayıda output</li><li>örnek:kanser tip taşhisi</li></ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <h4>Unsupervised Learning</h4>
+                    <p>herhangi bir önceden tanımlanmış hedef veya çıktı kullanılmadan bir veri kümesindeki kalıpları ve özellikleri keşfetmek için kullanılan bir tür makine öğrenimi yöntemidir.</p>
+                    <p>bunun altında 3 tip vardır:</p>
+                    <ul>
+                        <li>
+                            <h5>Clustering</h5>
+                            <ul><li>benzer veri noktalarını bir arada gruplandırır</li></ul>
+                        <li>
+                            <h5>Anomaly Detection</h5>
+                            <ul><li>olağan dışı veri noktaları bulma</li></ul>
+                        </li>
+                        <li>
+                            <h5>Dimensionality Reduction</h5>
+                            <ul><li>daha az sayı kullanarak verileri sıkıştırmax</li></ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </body>
+</html>
